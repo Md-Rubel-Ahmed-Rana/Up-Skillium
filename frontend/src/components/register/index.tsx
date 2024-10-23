690fcfc7dafce8fc346f2070558644e6435b050a
Mdnajimuddinhelal70 <najimuddinhelalal670@gmail.com>
2024-10-23 21:50:33 +0600

    added responsive design in login page and register page and removed bg color

--- frontend/src/components/register/index.tsx
+++ frontend/src/components/register/index.tsx
@@ -16,17 +16,11 @@ type FieldType = {
 };
 
 const Register: React.FC = () => {
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }, 
-  } = useForm<FieldType>();
-
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldType>();
   const [passwordErrors, setPasswordErrors] = useState<string[]>([]);
 
   const handleRegister: SubmitHandler<FieldType> = (data) => {
-     console.log(data)
+    console.log(data);
   };
 
   const handlePasswordChange = (password: string) => {
@@ -35,12 +29,14 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center bg-orange-300 h-screen">
+    <div className="flex items-center justify-center min-h-screen px-4">
       <form
-        className="bg-white w-1/3 shadow-lg rounded-lg space-y-8 p-8"
+        className="bg-gray-200 w-full max-w-lg shadow-lg rounded-lg space-y-8 p-8"
         onSubmit={handleSubmit(handleRegister)}
       >
-        <h2 className="text-2xl font-bold text-center text-gray-800">User Registration</h2>
+        <h2 className="text-2xl font-bold text-center font-serif text-gray-800">
+        Create Your Learning Account Today!
+        </h2>
 
         <div>
           <Input
@@ -48,7 +44,6 @@ const Register: React.FC = () => {
             {...register("name", {
               required: "Name is required",
               minLength: { value: 3, message: "Name must be at least 3 characters" },
-            
             })}
             placeholder="Name"
           />
@@ -62,41 +57,37 @@ const Register: React.FC = () => {
               required: "Email is required",
             })}
             placeholder="Email"
-          
           />
           {errors.email && <p className="text-red-500 text-xs">{errors.email.message}</p>}
         </div>
 
-        <div className="my-3 ">
-        <InputPassword
-          className="w-full"
-          {...register("password", {
-            required: "Password is required",
-            onChange: (e) => {
-              handlePasswordChange(e.target.value);
-            },
-          })}
-          placeholder="Password"
-        />
-        {errors.password && (
-          <p className="text-red-500 text-sm flex items-center gap-2">
-            {errors.password?.message}
-          </p>
-        )}
-        {!errors.password && passwordErrors.length > 0 && (
-          <div className="flex flex-col gap-1 mt-2">
-            {passwordErrors.map((error, index) => (
-              <p
-                className="text-red-500 text-sm flex items-center gap-2"
-                key={index}
-              >
-                <RxCross1 className="bg-red-500 text-white rounded-md p-1 text-lg" />
-                {error}
-              </p>
-            ))}
-          </div>
-        )}
-      </div>
+        <div className="my-3">
+          <InputPassword
+            className="w-full"
+            {...register("password", {
+              required: "Password is required",
+              onChange: (e) => {
+                handlePasswordChange(e.target.value);
+              },
+            })}
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm flex items-center gap-2">
+              {errors.password?.message}
+            </p>
+          )}
+          {!errors.password && passwordErrors.length > 0 && (
+            <div className="flex flex-col gap-1 mt-2">
+              {passwordErrors.map((error, index) => (
+                <p className="text-red-500 text-sm flex items-center gap-2" key={index}>
+                  <RxCross1 className="bg-red-500 text-white rounded-md p-1 text-lg" />
+                  {error}
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
 
         <Button type="primary" htmlType="submit" className="w-full">
           Register
@@ -105,7 +96,7 @@ const Register: React.FC = () => {
         <p className="text-center text-gray-600">
           Already have an account?{" "}
           <Link className="text-yellow-500 font-extrabold" href="/login">
-            {"=>>"} Login
+            Login
           </Link>
         </p>
       </form>
