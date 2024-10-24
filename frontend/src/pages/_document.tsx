4f6356d56fe869d49325d857e06a5e1ea2d0a13b
Mdnajimuddinhelal70 <najimuddinhelalal670@gmail.com>
2024-10-24 10:20:25 +0600

    added favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/educationalFav.png" type="image/webp" />
       <body className="antialiased">
         <Main />
         <NextScript />
