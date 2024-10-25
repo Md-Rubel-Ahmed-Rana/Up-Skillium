c28a26b36a5a9a0a462678be9d51dde52abe17f0
Mdnajimuddinhelal70 <najimuddinhelalal670@gmail.com>
2024-10-25 21:04:11 +0600

    added banner section

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,9 @@
+import Layout from "./HomePage/Layout";
+
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h2 className="text-4xl font-semibold text-blue-500">
-        Welcome to &quot;UpSkillium&quot;
-      </h2>
-    </div>
+   <div>
+      <Layout />
+   </div>
   );
 }
