7a9f800e48a3a249cdee54ab9d09b53755099df8
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-26 15:38:06 +0600

    category built

---
+++ backend/dist/modules/category/model.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Category = void 0;
+const mongoose_1 = require("mongoose");
+const categorySchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+    toJSON: {
+        versionKey: false,
+        virtuals: true,
+    },
+});
+exports.Category = (0, mongoose_1.model)("Category", categorySchema);
