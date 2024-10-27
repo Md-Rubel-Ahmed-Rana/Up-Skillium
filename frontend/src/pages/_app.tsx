3541cae7b29b982b44826269fbcb926a9edac027
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-27 16:55:34 +0600

    Redux setup completed

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,16 @@
+import store from "@/app/store";
 import Footer from "@/shared/Footer";
 import Navbar from "@/shared/Navbar";
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />;
       <Footer />
-    </>
-  )
+    </Provider>
+  );
 }
