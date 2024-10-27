f5766bad4e010bb2df51320656507a84365d5f05
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-27 22:30:51 +0600

    CORS policy enabled

--- backend/dist/middlewares/expressMiddlewares.js
+++ backend/dist/middlewares/expressMiddlewares.js
@@ -9,10 +9,11 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_2 = require("../config/cors");
 const expressMiddlewares = (app) => {
     app.use(express_1.default.json());
     app.use(express_1.default.urlencoded({ extended: true }));
-    app.use((0, cors_1.default)());
+    app.use((0, cors_1.default)(cors_2.corsConfig));
     app.use((0, helmet_1.default)());
     // app.use(apiRateLimiter.limitAPIRequest());
     app.use((0, cookie_parser_1.default)());
--- backend/src/middlewares/expressMiddlewares.ts
+++ backend/src/middlewares/expressMiddlewares.ts
@@ -2,16 +2,15 @@ import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import apiRateLimiter from "../config/apiRateLimiter";
 import cookieParser from "cookie-parser";
+import { corsConfig } from "../config/cors";
 
 export const expressMiddlewares = (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(cors(corsConfig));
   app.use(helmet());
   // app.use(apiRateLimiter.limitAPIRequest());
   app.use(cookieParser());
-
   app.use(morgan("dev"));
 };
