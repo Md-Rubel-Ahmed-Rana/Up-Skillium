08ef3d0d21d8e864db3ef4f2824b323394a2d4de
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-28 13:38:30 +0600

    Module APIs development completed

---
+++ backend/src/modules/module/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { ModuleController } from "./controller";
+
+const router = Router();
+
+router.post("/create", ModuleController.createNewModule);
+
+router.get("/", ModuleController.getAllModules);
+
+router.get("/:id", ModuleController.getSingleModule);
+
+router.get("/by-course/:courseId", ModuleController.getModuleByCourseId);
+
+router.patch("/:id", ModuleController.updateModule);
+
+router.delete("/:id", ModuleController.deleteModule);
+
+export const ModuleRoutes = router;
