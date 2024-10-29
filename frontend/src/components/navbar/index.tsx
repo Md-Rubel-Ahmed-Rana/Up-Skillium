8f94dda8b005af5c6b49c5453c7eea83a0a08644
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-29 16:50:40 +0600

    Project built and ready to deploy

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -11,7 +11,6 @@ const NavbarPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { data } = useGetLoggedInUserQuery({});
   const user = data?.data as IUser;
-  console.log(user);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
