8de376e3c04659b9302d7b911227dbb34e3db8ec
Mdnajimuddinhelal70 <najimuddinhelalal670@gmail.com>
2024-10-29 12:49:31 +0600

    integrated ant design in the register page

--- frontend/src/components/register/index.tsx
+++ frontend/src/components/register/index.tsx
@@ -1,16 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import { useForm, SubmitHandler } from "react-hook-form";
-import Link from "next/link";
-import verifyStrongPassword from "@/utils/verifyStrongPassword";
-import { RxCross1 } from "react-icons/rx";
-import { useCreateStudentMutation } from "@/features/student";
-import toast from "react-hot-toast";
-
-const Button = dynamic(() => import("antd/lib/button"), { ssr: false });
-const Input = dynamic(() => import("antd/lib/input"), { ssr: false });
-const InputPassword = dynamic(() => import("antd/lib/input/Password"), { ssr: false });
+import React from 'react';
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd/lib';
+import toast from 'react-hot-toast';
+import { useCreateStudentMutation } from '@/features/student';
 
 type FieldType = {
   name: string;
@@ -19,21 +12,15 @@ type FieldType = {
 };
 
 const Register: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FieldType>({ mode: "onChange" });
-  const [passwordErrors, setPasswordErrors] = useState<string[]>([]);
-  const [createStudent, { isLoading }] = useCreateStudentMutation();
+  const [studentRegister, { isLoading }] = useCreateStudentMutation();
 
-  // api related code
-  const handleRegister: SubmitHandler<FieldType> = async (data) => {
-    console.log(data);
+  const onFinish: FormProps<FieldType>['onFinish'] = async (data) => {
+    console.log('Success:', data);
+
     try {
-      const response: any = await createStudent(data);
+      const response: any = await studentRegister(data);
       if (response?.data?.statusCode === 201) {
-        toast.success(response?.data?.message || "Create Student successfully");
+        toast.success(response?.data?.message || "User registration successful");
       } else {
         toast.error(
           response?.data?.message ||
@@ -46,93 +33,46 @@ const Register: React.FC = () => {
     }
   };
 
-  const handlePasswordChange = (password: string) => {
-    const errors = verifyStrongPassword(password);
-    setPasswordErrors(errors);
-  };
-
   return (
-    <div className="flex items-center justify-center min-h-screen px-4">
-      <form
-        className="bg-gray-200 w-full max-w-lg shadow-lg rounded-lg space-y-8 p-8"
-        onSubmit={handleSubmit(handleRegister)}
+    <div className='pt-20 flex items-center justify-center h-screen'>
+      <Form
+      className='bg-slate-200 p-6 rounded-lg shadow-lg w-full max-w-md space-y-6'
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
       >
-        <h2 className="text-2xl font-bold text-center font-serif text-gray-800">
-          Create Your Learning Account Today!
-        </h2>
+        <Form.Item<FieldType>
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: 'Name is required' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Email is required' }]}
+        >
+          <Input />
+        </Form.Item>
 
-        <div>
-          <input
-            className="w-full"
-            {...register("name", {
-              required: "Name is required",
-              minLength: {
-                value: 3,
-                message: "Name must be at least 3 characters",
-              },
-            })}
-            placeholder="Name"
-          />
-          {errors.name && (
-            <p className="text-red-500 text-xs">{errors.name.message}</p>
-          )}
-        </div>
-
-        <div>
-          <input
-            className="w-full"
-            {...register("email", {
-              required: "Email is required",
-            })}
-            placeholder="Email"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs">{errors.email.message}</p>
-          )}
-        </div>
-
-        <div className="my-3">
-          <input
-            className="w-full"
-            {...register("password", {
-              required: "Password is required",
-              onChange: (e) => {
-                handlePasswordChange(e.target.value);
-              },
-            })}
-            placeholder="Password"
-          />
-          {errors.password && (
-            <p className="text-red-500 text-sm flex items-center gap-2">
-              {errors.password?.message}
-            </p>
-          )}
-          {!errors.password && passwordErrors.length > 0 && (
-            <div className="flex flex-col gap-1 mt-2">
-              {passwordErrors.map((error, index) => (
-                <p
-                  className="text-red-500 text-sm flex items-center gap-2"
-                  key={index}
-                >
-                  <RxCross1 className="bg-red-500 text-white rounded-md p-1 text-lg" />
-                  {error}
-                </p>
-              ))}
-            </div>
-          )}
-        </div>
-
-        <Button type="primary" htmlType="submit" className="w-full">
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Password is required' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit" loading={isLoading}>
           Register
-        </Button>
-
-        <p className="text-center text-gray-600">
-          Already have an account?{" "}
-          <Link className="text-yellow-500 font-extrabold" href="/login">
-            Login
-          </Link>
-        </p>
-      </form>
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
