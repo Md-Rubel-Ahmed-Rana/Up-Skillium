5c5ab4b388d1333bb65e17037d3f33411def3d5d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-10-30 15:26:17 +0600

    Profile picture edit icon added on Profile Card's image

--- frontend/src/components/dashboard/profile/index.tsx
+++ frontend/src/components/dashboard/profile/index.tsx
@@ -1,5 +1,3 @@
-import UpdateProfilePicture from "./UpdateProfilePicture";
-import { useState } from "react";
 import DashboardLayout from "@/layout/DashboardLayout";
 import { FaUserCircle } from "react-icons/fa";
 import { CiSettings } from "react-icons/ci";
@@ -7,8 +5,6 @@ import Link from "next/link";
 import ProfileInfo from "./ProfileInfo";
 
 const Profile = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const sidebarItems = [
     {
       key: "1",
@@ -24,11 +20,7 @@ const Profile = () => {
 
   return (
     <DashboardLayout sidebarItems={sidebarItems}>
-      {/* <Button onClick={() => setIsModalOpen(true)} type="primary">
-        Change Profile Picture
-      </Button> */}
       <ProfileInfo />
-      <UpdateProfilePicture isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
     </DashboardLayout>
   );
 };
