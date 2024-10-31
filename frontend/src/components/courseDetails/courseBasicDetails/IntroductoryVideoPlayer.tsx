9653aee11f72019884eafe57e370a1af8530ed98
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-01 01:04:31 +0600

    Course details page design completed

---
+++ frontend/src/components/courseDetails/courseBasicDetails/IntroductoryVideoPlayer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import { Card } from "antd/lib";
+
+type Props = {
+  videoUrl: string;
+};
+
+const IntroductoryVideoPlayer = ({ videoUrl }: Props) => {
+  return (
+    <Card
+      className="w-full rounded-lg overflow-hidden ring-2 bg-blue-100"
+      styles={{ body: { padding: "0px" } }}
+      bordered={false}
+    >
+      <div className="relative pb-[56.25%]">
+        <ReactPlayer
+          url={videoUrl}
+          width="100%"
+          height="100%"
+          controls
+          className="absolute object-cover top-0 left-0 rounded-lg overflow-hidden shadow-lg"
+        />
+      </div>
+    </Card>
+  );
+};
+
+export default IntroductoryVideoPlayer;
--- frontend/src/components/courseDetails/courseBasicDetails/index.tsx
+++ frontend/src/components/courseDetails/courseBasicDetails/index.tsx
@@ -1,5 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import { useGetSingleCourseQuery } from "@/features/course";
+import IntroductoryVideoPlayer from "./IntroductoryVideoPlayer";
+import { useRouter } from "next/router";
+import { ICourse } from "@/types/course.type";
+import CourseTitleCategoryCard from "./CourseTitleCategoryCard";
+import InstructorCard from "./InstructorCard";
+import CourseInformation from "./CourseInformation";
+import StudentFeedback from "./StudentFeedback";
+
+const url =
+  "https://firebasestorage.googleapis.com/v0/b/up-skillium.appspot.com/o/up-skillium%2Fcourse-introductory-videos%2FShibir-jannati-dol.mp4?alt=media&token=138383d6-7d05-4579-a0a7-88de21fa291e";
+
 const CourseBasicDetails = () => {
-  return <div>CourseBasicDetails</div>;
+  const { query } = useRouter();
+  const courseId = query?.courseId as string;
+  const { data } = useGetSingleCourseQuery({ id: courseId });
+  const course = data?.data as ICourse;
+
+  return (
+    <div className="p-6 max-w-4xl mx-auto space-y-6 bg-gray-50 rounded-lg shadow-md">
+      <IntroductoryVideoPlayer videoUrl={course?.introductoryVideo || url} />
+
+      <CourseTitleCategoryCard
+        title={course?.title}
+        category={course?.category}
+      />
+
+      <InstructorCard instructor={course?.instructor} />
+
+      <CourseInformation course={course} />
+
+      <StudentFeedback course={course} />
+    </div>
+  );
 };
 
 export default CourseBasicDetails;
