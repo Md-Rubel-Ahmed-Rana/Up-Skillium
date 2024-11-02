98088cc6e7766084633f5eca894d64de7e0aaeaa
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-02 23:47:04 +0600

    Spacing problem solved

--- frontend/src/layout/DashboardLayout.tsx
+++ frontend/src/layout/DashboardLayout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { Layout } from "antd/lib";
 import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
+import { useMediaQuery } from "react-responsive";
 const { Content, Sider } = Layout;
 
 type DashboardLayoutProps = {
@@ -8,13 +9,17 @@ type DashboardLayoutProps = {
 };
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  const isLargeDevice = useMediaQuery({ minWidth: 1024 });
   return (
-    <Layout hasSider>
-      <Sider className="bg-white">
+    <Layout className="lg:mt-2" hasSider={isLargeDevice}>
+      <Sider
+        style={{ display: isLargeDevice ? "block" : "none" }}
+        className="bg-white"
+      >
         <DashboardSidebar />
       </Sider>
-      <Layout>
-        <Content className="max-h-screen h-full overflow-y-auto">
+      <Layout className="bg-white ">
+        <Content className="max-h-screen h-full  overflow-y-auto">
           {children}
         </Content>
       </Layout>
