640d5ca5298dda18da25d9d2ec3de93a0149b602
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-07 23:03:50 +0600

    Selected quiz answers ready to send backend to calculate result

--- frontend/src/components/modules/ShowQuizQuestions.tsx
+++ frontend/src/components/modules/ShowQuizQuestions.tsx
@@ -7,40 +7,64 @@ type Props = {
   lesson: ILesson;
 };
 
+type ISelectedAnswers = {
+  id: string;
+  answer: string;
+};
+
 const ShowQuizQuestions = ({ lesson }: Props) => {
   const questions = lesson?.quizQuestions;
+  const [selectedAnswers, setSelectedAnswers] = useState<ISelectedAnswers[]>(
+    []
+  );
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [question, setQuestion] = useState(questions[currentIndex]);
 
-  const handleOptionClick = (option: string) => {
+  const handleSelectOption = (option: string) => {
     setSelectedOption(option);
+
+    const selectedAnswer = { id: question.id, answer: option };
+    setSelectedAnswers((prevAnswers) => {
+      const existingAnswerIndex = prevAnswers.findIndex(
+        (answer) => answer.id === question.id
+      );
+      if (existingAnswerIndex !== -1) {
+        const updatedAnswers = [...prevAnswers];
+        updatedAnswers[existingAnswerIndex] = selectedAnswer;
+        return updatedAnswers;
+      } else {
+        return [...prevAnswers, selectedAnswer];
+      }
+    });
   };
 
   const handleSubmitQuiz = () => {
     toast.success("Quiz submitted");
+    console.log("Selected Answers:", selectedAnswers);
   };
 
   useEffect(() => {
     setQuestion(questions[currentIndex]);
-  }, [currentIndex, questions]);
+    setSelectedOption(
+      selectedAnswers.find((answer) => answer.id === question?.id)?.answer ||
+        null
+    );
+  }, [currentIndex, questions, selectedAnswers, question?.id]);
 
   return (
     <div className="bg-white shadow-md rounded-lg p-6">
       <h2 className="text-xl font-semibold mb-4">{question?.question}</h2>
-      <div className="space-y-3">
+      <div className="flex flex-col justify-start items-start space-y-3 text-center">
         {question?.options?.map((option, index) => (
-          <button
+          <Button
             key={index}
-            onClick={() => handleOptionClick(option)}
-            className={`w-full text-left py-2 px-4 border rounded-lg ${
-              selectedOption === option
-                ? "bg-blue-500 text-white border-blue-500"
-                : "border-gray-300 hover:bg-blue-100"
-            }`}
+            type={`${selectedOption === option ? "primary" : "default"}`}
+            onClick={() => handleSelectOption(option)}
+            className={`w-full flex justify-start items-start`}
           >
             {option}
-          </button>
+          </Button>
         ))}
       </div>
       <div className="text-right my-2 pt-4">
