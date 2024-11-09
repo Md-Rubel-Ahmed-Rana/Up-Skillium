2c2bbc901df7569a6162eda6acde6d3ad930ab3f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-09 17:13:58 +0600

    Course loading skeleton design completed

--- frontend/src/components/dashboard/my-courses/student/index.tsx
+++ frontend/src/components/dashboard/my-courses/student/index.tsx
@@ -3,20 +3,29 @@ import { useMyCoursesQuery } from "@/features/student";
 import { IMyCourse } from "@/types/course.type";
 import { IUser } from "@/types/user.type";
 import MyCourseCard from "./MyCourseCard";
+import MyCourseSkeleton from "@/skeletons/CourseSkeleton";
 
 const MyCourses = () => {
   const { data } = useGetLoggedInUserQuery({});
   const user = data?.data as IUser;
-  const { data: courseData } = useMyCoursesQuery({ userId: user?.id });
+  const { data: courseData, isLoading } = useMyCoursesQuery({
+    userId: user?.id,
+  });
   const courses =
     ((courseData?.data?.courses ||
       courseData?.data?.coursesEnrolled) as IMyCourse[]) || [];
   return (
-    <div className="mt-5">
-      {courses?.map((course) => (
-        <MyCourseCard key={course?.id} course={course} />
-      ))}
-    </div>
+    <>
+      {isLoading ? (
+        <MyCourseSkeleton />
+      ) : (
+        <div className="mt-5">
+          {courses?.map((course) => (
+            <MyCourseCard key={course?.id} course={course} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
