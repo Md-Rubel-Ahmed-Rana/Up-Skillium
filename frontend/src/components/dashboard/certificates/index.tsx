106fdde0fce53647d8ffd5e8c11ac6c20b099808
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-09 17:25:27 +0600

    Certificate loading skeleton design done

--- frontend/src/components/dashboard/certificates/index.tsx
+++ frontend/src/components/dashboard/certificates/index.tsx
@@ -1,5 +1,6 @@
 import { useGetLoggedInUserQuery } from "@/features/auth";
 import { useGetStudentCertificatesQuery } from "@/features/certificate";
+import CertificateSkeleton from "@/skeletons/CertificateSkeleton";
 import { IGetCertificate } from "@/types/certificate.type";
 import { IUser } from "@/types/user.type";
 import { Button, Typography } from "antd/lib";
@@ -9,7 +10,9 @@ const { Title, Text } = Typography;
 const StudentCertificates = () => {
   const { data: userData } = useGetLoggedInUserQuery({});
   const user = userData?.data as IUser;
-  const { data } = useGetStudentCertificatesQuery({ userId: user?.id });
+  const { data, isLoading } = useGetStudentCertificatesQuery({
+    userId: user?.id,
+  });
   const certificates = data?.data as IGetCertificate[];
 
   return (
@@ -17,31 +20,38 @@ const StudentCertificates = () => {
       <Title level={3} className="text-gray-600">
         Student Certificates
       </Title>
-      {certificates?.length > 0 ? (
-        <ul className="flex flex-col gap-2">
-          {certificates?.map((certificate) => (
-            <li key={certificate?.id} className="p-4 border rounded">
-              <div className="flex items-center justify-between">
-                <h3 className="text-lg font-semibold">
-                  {certificate?.course?.title || "Web Development Masterclass"}
-                </h3>
-                <Button
-                  type="primary"
-                  size="large"
-                  onClick={() =>
-                    window.open(certificate?.certificateUrl, "_blank")
-                  }
-                >
-                  Download
-                </Button>
-              </div>
-            </li>
-          ))}
-        </ul>
+      {isLoading ? (
+        <CertificateSkeleton />
       ) : (
-        <Text className="font-semibold text-lg text-gray-600">
-          No certificates available.
-        </Text>
+        <>
+          {certificates?.length > 0 ? (
+            <ul className="flex flex-col gap-2">
+              {certificates?.map((certificate) => (
+                <li key={certificate?.id} className="p-4 border rounded">
+                  <div className="flex items-center justify-between">
+                    <h3 className="text-lg font-semibold">
+                      {certificate?.course?.title ||
+                        "Web Development Masterclass"}
+                    </h3>
+                    <Button
+                      type="primary"
+                      size="large"
+                      onClick={() =>
+                        window.open(certificate?.certificateUrl, "_blank")
+                      }
+                    >
+                      Download
+                    </Button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <Text className="font-semibold text-lg text-gray-600">
+              No certificates available.
+            </Text>
+          )}
+        </>
       )}
     </div>
   );
