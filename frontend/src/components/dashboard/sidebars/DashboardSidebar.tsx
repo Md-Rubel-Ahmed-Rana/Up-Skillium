9c31ae3fcbff2f15bf67659925a00beb8805a992
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-09 15:32:43 +0600

    dashboard sidebar data loading skeleton impletemented

--- frontend/src/components/dashboard/sidebars/DashboardSidebar.tsx
+++ frontend/src/components/dashboard/sidebars/DashboardSidebar.tsx
@@ -3,16 +3,23 @@ import { IUser } from "@/types/user.type";
 import StudentSidebar from "./StudentSidebar";
 import AdminSidebar from "./AdminSidebar";
 import InstructorSidebar from "./InstructorSidebar";
+import DashboardSidebarSkeleton from "@/skeletons/DashboardSidebarSkeleton";
 
 const DashboardSidebar = () => {
-  const { data } = useGetLoggedInUserQuery({});
+  const { data, isLoading } = useGetLoggedInUserQuery({});
   const user = data?.data as IUser;
   return (
-    <div>
-      {user?.role?.role === "student" && <StudentSidebar />}
-      {user?.role?.role === "admin" && <AdminSidebar />}
-      {user?.role?.role === "instructor" && <InstructorSidebar />}
-    </div>
+    <>
+      {isLoading ? (
+        <DashboardSidebarSkeleton />
+      ) : (
+        <div>
+          {user?.role?.role === "student" && <StudentSidebar />}
+          {user?.role?.role === "admin" && <AdminSidebar />}
+          {user?.role?.role === "instructor" && <InstructorSidebar />}
+        </div>
+      )}
+    </>
   );
 };
 
