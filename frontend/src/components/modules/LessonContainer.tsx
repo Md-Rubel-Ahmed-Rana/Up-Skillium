d95da96dea84ae5f48bb5ef40c05f254e83c8e0a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-09 17:42:17 +0600

    Single lesson loading skeleton design completed

--- frontend/src/components/modules/LessonContainer.tsx
+++ frontend/src/components/modules/LessonContainer.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ILesson } from "@/types/lesson.type";
 import LessonVideoPlayer from "./LessonVideoPlayer";
 import ShowAssignment from "./ShowAssignment";
@@ -6,6 +7,7 @@ import ShowInstruction from "./ShowInstruction";
 import LessonActions from "./LessonActions";
 import { useGetSingleLessonQuery } from "@/features/lesson";
 import { ILessonProgress } from "@/types/studentProgress.type";
+import SingleLessonSkeleton from "@/skeletons/SingleLessonSkeleton";
 
 type Props = {
   lessonId: string;
@@ -14,36 +16,53 @@ type Props = {
 };
 
 const LessonContainer = ({ lessonId, setLessonId, lessons }: Props) => {
-  const { data } = useGetSingleLessonQuery({ lessonId: lessonId });
+  const { data, isLoading, refetch } = useGetSingleLessonQuery(
+    { lessonId },
+    { refetchOnMountOrArgChange: true }
+  );
   const lesson = data?.data as ILesson;
   const findCurrentLesson = lessons.find((ls) => ls?.lesson?.id === lessonId);
+
+  useEffect(() => {
+    refetch();
+  }, [lessonId, refetch]);
+  console.log({ isLoading });
+
   return (
-    <div className="min-h-screen">
-      <h2 className="text-2xl font-semibold mb-3">{lesson?.title}</h2>
-      {lesson?.type === "video" && (
-        <LessonVideoPlayer videoUrl={lesson?.videoUrl} />
+    <>
+      {isLoading ? (
+        <SingleLessonSkeleton />
+      ) : (
+        <div className="min-h-screen">
+          <h2 className="text-2xl font-semibold mb-3">{lesson?.title}</h2>
+          {lesson?.type === "video" && (
+            <LessonVideoPlayer videoUrl={lesson?.videoUrl} />
+          )}
+          {lesson?.type === "assignment" && (
+            <ShowAssignment
+              lesson={lesson}
+              isAssignmentSubmitted={
+                findCurrentLesson?.isAssignmentSubmitted as boolean
+              }
+            />
+          )}
+          {lesson?.type === "quiz" && (
+            <ShowQuizQuestions
+              lesson={lesson}
+              isQuizSubmitted={findCurrentLesson?.isQuizSubmitted as boolean}
+            />
+          )}
+          {lesson?.type === "instruction" && (
+            <ShowInstruction lesson={lesson} />
+          )}
+          <LessonActions
+            lessonId={lessonId}
+            setLessonId={setLessonId}
+            lessons={lessons}
+          />
+        </div>
       )}
-      {lesson?.type === "assignment" && (
-        <ShowAssignment
-          lesson={lesson}
-          isAssignmentSubmitted={
-            findCurrentLesson?.isAssignmentSubmitted as boolean
-          }
-        />
-      )}
-      {lesson?.type === "quiz" && (
-        <ShowQuizQuestions
-          lesson={lesson}
-          isQuizSubmitted={findCurrentLesson?.isQuizSubmitted as boolean}
-        />
-      )}
-      {lesson?.type === "instruction" && <ShowInstruction lesson={lesson} />}
-      <LessonActions
-        lessonId={lessonId}
-        setLessonId={setLessonId}
-        lessons={lessons}
-      />
-    </div>
+    </>
   );
 };
 
