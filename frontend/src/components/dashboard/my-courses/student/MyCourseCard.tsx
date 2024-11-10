f1991020b2ab26dbcd1c2cf5e57cfe1fdbfb1600
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-10 18:15:41 +0600

    Getting student's my courses from course progress and showing done

--- frontend/src/components/dashboard/my-courses/student/MyCourseCard.tsx
+++ frontend/src/components/dashboard/my-courses/student/MyCourseCard.tsx
@@ -1,38 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
-import { IMyCourse } from "@/types/course.type";
-import { Button, Card } from "antd/lib";
+import { ICourseProgress } from "@/types/studentProgress.type";
+import makeLessonTitleAsParamsUrl from "@/utils/makeLessonTitleAsParamsUrl";
+import { Button, Card, Progress } from "antd/lib";
+import { FaRegCheckCircle } from "react-icons/fa";
 import Link from "next/link";
 
 const { Meta } = Card;
 
 type Props = {
-  course: IMyCourse;
+  course: ICourseProgress;
 };
 
 const MyCourseCard = ({ course }: Props) => {
+  const isCompleted = course?.completionPercentage === 100;
+
   return (
     <Card
       style={{ width: 300 }}
-      cover={<img alt={course?.title} src={course?.image} />}
+      cover={<img alt={course?.course?.title} src={course?.course?.image} />}
       actions={[
         <Link
-          // href={`/modules/classes/${course?.id}?courseId=${course?.id}&courseTitle=${course?.title}`}
-          href={`/classes/${course?.id}`}
+          href={`/classes/${course?.course?.id}/${
+            course?.lastCompletedLesson?.id
+          }/${makeLessonTitleAsParamsUrl(course?.lastCompletedLesson?.title)}`}
           key={"1"}
         >
-          <Button variant="filled" type="primary">
-            Continue classes
-          </Button>
+          <Button type="primary">Continue Classes</Button>
         </Link>,
         <Link
-          href={`/courses/details/${course?.id}?courseId=${course?.id}&courseTitle=${course?.title}`}
+          href={`/courses/details/${course?.course?.id}?courseId=${course?.course?.id}&courseTitle=${course?.course?.title}`}
           key={"2"}
         >
-          <Button>Course Outline</Button>,
+          <Button>Course Outline</Button>
         </Link>,
       ]}
     >
-      <Meta title={course?.title} />
+      <Meta title={course?.course?.title} />
+
+      <div className="mt-3">
+        <Progress
+          percent={course?.completionPercentage}
+          size={[250, 15]}
+          status={isCompleted ? "success" : "active"}
+        />
+        {isCompleted && (
+          <div className="flex items-center justify-center text-green-600 mt-2">
+            <FaRegCheckCircle className="mr-2 text-xl" />
+            <span className="text-xl font-semibold">Course Completed</span>
+          </div>
+        )}
+      </div>
     </Card>
   );
 };
