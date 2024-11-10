671644aae6685d9dec15444ff58ca85e574ea5ea
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-10 12:27:30 +0600

    Dashboard sidebar loading skeleton issue solved

--- frontend/src/skeletons/dashboardSidebarSkeleton/index.tsx
+++ frontend/src/skeletons/dashboardSidebarSkeleton/index.tsx
@@ -4,7 +4,7 @@ const DashboardSidebarSkeleton = () => {
   return (
     <div
       style={{ width: "200px" }}
-      className="flex w-full min-w-6 flex-col  items-center gap-5 p-2 mt-4"
+      className="flex lg:w-full w-screen min-w-6 lg:flex-col  items-center gap-5 p-2 mt-4 flex-row overflow-x-auto lg:overflow-visible space-x-3"
     >
       {Array.from({ length: 20 }).map((item, index) => (
         <div key={index} className="flex items-center gap-2 w-full">
