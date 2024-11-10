f1d742a6ff894a7d56bfd51af70ebc595f032363
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-10 12:27:30 +0600

    Dashboard sidebar loading skeleton issue solved

--- frontend/src/components/modules/index.tsx
+++ frontend/src/components/modules/index.tsx
@@ -36,7 +36,7 @@ const ModulesClasses = () => {
   return (
     <div className="flex flex-col-reverse lg:flex-row justify-between lg:gap-5 gap-20 mt-5 min-h-screen p-2">
       <div className="w-full lg:w-4/12">
-        {!isLoading ? (
+        {isLoading ? (
           <ModuleListSkeleton />
         ) : (
           <ModuleList
