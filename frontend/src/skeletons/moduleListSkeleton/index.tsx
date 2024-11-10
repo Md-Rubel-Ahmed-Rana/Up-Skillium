26c0ac8396128ccabc7f82974ef6a4b60737b731
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-10 12:08:22 +0600

    Module and lesson list loading skeleton design and implementation done

--- frontend/src/skeletons/moduleListSkeleton/index.tsx
+++ frontend/src/skeletons/moduleListSkeleton/index.tsx
@@ -1,11 +1,19 @@
 import CourseProgressLessonCountSkeleton from "./CourseProgressLessonCountSkeleton";
+import LessonListSkeleton from "./LessonListSkeleton";
 import LessonSearchSkeleton from "./LessonSearchSkeleton";
+import ModuleSkeleton from "./ModuleSkeleton";
 
 const ModuleListSkeleton = () => {
   return (
-    <div className="border rounded-md">
+    <div className="border rounded-md h-screen overflow-y-auto">
       <CourseProgressLessonCountSkeleton />
       <LessonSearchSkeleton />
+      <ModuleSkeleton />
+      <LessonListSkeleton />
+      <ModuleSkeleton />
+      <LessonListSkeleton />
+      <ModuleSkeleton />
+      <LessonListSkeleton />
     </div>
   );
 };
