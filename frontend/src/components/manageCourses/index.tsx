19ecdfb006e419e19013a4d1bddd72247f81da43
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-12 11:32:01 +0600

    Courses list view mode component design done

--- frontend/src/components/manageCourses/index.tsx
+++ frontend/src/components/manageCourses/index.tsx
@@ -1,10 +1,13 @@
 import { useGetAllCoursesQuery } from "@/features/course";
 import { ICourse } from "@/types/course.type";
 import ManageCourseCard from "./ManageCourseCard";
+import { useState } from "react";
+import LayoutSwitcher from "./LayoutSwitcher";
 
 const ManageCourses = () => {
   const { data, isLoading } = useGetAllCoursesQuery({});
   const courses = data?.data as ICourse[];
+  const [viewMode, setViewMode] = useState("grid");
   return (
     <div className="mt-4 pb-20">
       {isLoading ? (
@@ -14,11 +17,18 @@ const ManageCourses = () => {
       ) : (
         <>
           {courses?.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {courses.map((course) => (
-                <ManageCourseCard course={course} key={course?.id} />
-              ))}
-            </div>
+            <>
+              <LayoutSwitcher viewMode={viewMode} setViewMode={setViewMode} />
+              {viewMode === "grid" ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  {courses.map((course) => (
+                    <ManageCourseCard course={course} key={course?.id} />
+                  ))}
+                </div>
+              ) : (
+                <div>Show as table</div>
+              )}
+            </>
           ) : (
             <div className="h-screen flex justify-center items-center">
               <h1 className="text-2xl font-bold">No course found!</h1>
