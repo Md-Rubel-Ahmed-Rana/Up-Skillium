4c92b3c8476a5fd20e67243289bbe83a7f5b1514
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-12 10:14:27 +0600

    Admin sidebar design done

--- frontend/src/components/dashboard/sidebars/AdminSidebar.tsx
+++ frontend/src/components/dashboard/sidebars/AdminSidebar.tsx
@@ -1,70 +1,85 @@
+import LogoutButton from "@/components/shared/LogoutButton";
 import { Menu, MenuProps } from "antd/lib";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import {
   FaUserCircle,
   FaChalkboardTeacher,
   FaBook,
   FaUsers,
   FaChartBar,
-  FaCogs,
   FaCertificate,
   FaComments,
   FaSignOutAlt,
 } from "react-icons/fa";
 
 const AdminSidebar = () => {
+  const router = useRouter();
+
+  const pathToKeyMap: { [key: string]: string } = {
+    "/dashboard/profile": "1",
+    "/dashboard/manage-instructors": "2",
+    "/dashboard/manage-students": "3",
+    "/dashboard/manage-courses": "4",
+    "/dashboard/manage-certificates": "5",
+    "/dashboard/reports": "6",
+    "/dashboard/feedback-reviews": "7",
+  };
+
+  const selectedKey = pathToKeyMap[router.pathname] || "1";
+
   const adminItems: MenuProps["items"] = [
     {
       key: "1",
       icon: <FaUserCircle />,
-      label: "Profile",
+      label: <Link href={"/dashboard/profile"}>Profile</Link>,
     },
     {
       key: "2",
       icon: <FaChalkboardTeacher />,
-      label: "Manage Instructors",
+      label: (
+        <Link href={"/dashboard/manage-instructors"}>Manage Instructors</Link>
+      ),
     },
     {
       key: "3",
       icon: <FaUsers />,
-      label: "Manage Students",
+      label: <Link href={"/dashboard/manage-students"}>Manage Students</Link>,
     },
     {
       key: "4",
       icon: <FaBook />,
-      label: "Manage Courses",
+      label: <Link href={"/dashboard/manage-courses"}>Manage Courses</Link>,
     },
     {
       key: "5",
       icon: <FaCertificate />,
-      label: "Certificates",
+      label: <Link href={"/dashboard/manage-certificates"}>Certificates</Link>,
     },
     {
       key: "6",
       icon: <FaChartBar />,
-      label: "Reports",
+      label: <Link href={"/dashboard/reports"}>Reports</Link>,
     },
     {
       key: "7",
       icon: <FaComments />,
-      label: "Feedback & Reviews",
+      label: (
+        <Link href={"/dashboard/feedback-reviews"}>Feedback & Reviews</Link>
+      ),
     },
     {
       key: "8",
-      icon: <FaCogs />,
-      label: "Settings",
-    },
-    {
-      key: "9",
       icon: <FaSignOutAlt />,
-      label: "Logout",
+      label: <LogoutButton />,
     },
   ];
 
   return (
     <Menu
       theme="light"
-      mode="inline"
-      defaultSelectedKeys={["1"]}
+      className="w-full lg:min-h-screen h-full mt-3 flex lg:flex-col flex-row overflow-x-auto lg:overflow-visible space-x-3 lg:space-x-0"
+      defaultSelectedKeys={[selectedKey]}
       items={adminItems}
     />
   );
