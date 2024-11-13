b0e13a4e212939be5ec189210acd2e675546930c
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-13 16:47:50 +0600

    Course list dropdwon added on course outline page

--- frontend/src/components/dashboardCourseOutline/index.tsx
+++ frontend/src/components/dashboardCourseOutline/index.tsx
@@ -2,6 +2,7 @@ import { useGetCourseOutlineByCourseIdQuery } from "@/features/courseOutline";
 import { ICourseOutline } from "@/types/courseOutline.type";
 import { useRouter } from "next/router";
 import ModuleContainer from "./ModuleContainer";
+import CourseDropDownList from "./CourseDropDownList";
 
 const DashboardCourseOutlines = () => {
   const { query } = useRouter();
@@ -10,9 +11,10 @@ const DashboardCourseOutlines = () => {
   const outline = data?.data as ICourseOutline;
   return (
     <div className="pb-20">
-      <h1 className="text-2xl font-semibold text-center mt-3">
+      <h1 className="text-2xl font-semibold text-center my-2">
         Outline of the Course
       </h1>
+      <CourseDropDownList />
       <h2 className="text-xl font-semibold text-center my-2 text-gray-600">
         {outline?.course?.title}
       </h2>
