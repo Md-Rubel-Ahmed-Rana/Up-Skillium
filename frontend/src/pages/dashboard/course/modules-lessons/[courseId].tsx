351770885e4bafabc6472939dbc2db3a6bbed6f0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-14 01:24:25 +0600

    Course modules and lessons showing on admin dashboard completed

---
+++ frontend/src/pages/dashboard/course/modules-lessons/[courseId].tsx
@@ -0,0 +1,34 @@
+import DashboardModulesLessons from "@/components/dashboardModulesLessons";
+import DashboardLayout from "@/layout/DashboardLayout";
+import RootLayout from "@/layout/RootLayout";
+import PageMetadata from "@/utils/PageMetadata";
+import { useRouter } from "next/router";
+import React, { ReactElement } from "react";
+
+const DashboardModulesLessonsPage = () => {
+  const { query } = useRouter();
+  const courseTitle = query?.courseTitle as string;
+
+  return (
+    <>
+      <PageMetadata
+        title={`Course Modules & Lessons - ${
+          courseTitle || "Course title"
+        } - Up Skillium`}
+        description="this is up skillium home page"
+        keywords="up skillium, online course, web development, digital marketing"
+      />
+      <DashboardModulesLessons />
+    </>
+  );
+};
+
+DashboardModulesLessonsPage.getLayout = function (page: ReactElement) {
+  return (
+    <RootLayout>
+      <DashboardLayout>{page}</DashboardLayout>
+    </RootLayout>
+  );
+};
+
+export default DashboardModulesLessonsPage;
