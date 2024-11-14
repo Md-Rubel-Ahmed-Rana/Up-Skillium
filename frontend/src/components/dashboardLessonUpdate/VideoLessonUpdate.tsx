f97f3f61107402487d68d4f62fb16badae2c01fe
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-14 16:39:50 +0600

    Lesson update API integration completed

--- frontend/src/components/dashboardLessonUpdate/VideoLessonUpdate.tsx
+++ frontend/src/components/dashboardLessonUpdate/VideoLessonUpdate.tsx
@@ -3,6 +3,8 @@ import { ILesson } from "@/types/lesson.type";
 import { Form, Input, Button } from "antd/lib";
 import { useEffect, useState } from "react";
 import LessonVideoUploadModal from "./LessonVideoUploadModal";
+import { useUpdateLessonMutation } from "@/features/lesson";
+import toast from "react-hot-toast";
 
 type Props = {
   lesson: ILesson;
@@ -12,10 +14,38 @@ const VideoLessonUpdate = ({ lesson }: Props) => {
   const [form] = Form.useForm();
   const [uploadVideo, setUploadVideo] = useState(false);
   const [newVideoUrl, setNewVideoUrl] = useState("");
+  const [updateLesson, { isLoading }] = useUpdateLessonMutation();
   console.log(newVideoUrl);
 
-  const handleUpdateVideoLesson = (values: any) => {
-    console.log("Updated Data:", values);
+  const handleUpdateVideoLesson = async (values: ILesson) => {
+    const newData: ILesson = {
+      ...lesson,
+      title: values?.title,
+      serial: Number(values?.serial),
+      videoUrl: values?.videoUrl,
+      videoLength: Number(values?.videoLength),
+    };
+
+    try {
+      const result: any = await updateLesson({
+        lessonId: lesson?.id,
+        data: newData,
+      });
+      if (result?.data?.statusCode === 200) {
+        toast.success(
+          result?.data?.message || "Lesson was updated successfully!"
+        );
+      } else {
+        toast.error(
+          result?.error?.message ||
+            result?.error?.data?.message ||
+            result?.data?.error?.message ||
+            "Failed to update lesson"
+        );
+      }
+    } catch (error: any) {
+      toast.error(`Failed to update lesson. Error: ${error?.message}`);
+    }
   };
 
   useEffect(() => {
@@ -30,71 +60,86 @@ const VideoLessonUpdate = ({ lesson }: Props) => {
   }, [lesson, form]);
 
   return (
-    <Form
-      form={form}
-      onFinish={handleUpdateVideoLesson}
-      layout="vertical"
-      className="space-y-4 border p-4 rounded-md"
-    >
-      <Form.Item
-        label="Lesson Title"
-        name="title"
-        rules={[{ required: true, message: "Title is required" }]}
+    <>
+      <Form
+        form={form}
+        onFinish={handleUpdateVideoLesson}
+        layout="vertical"
+        className="space-y-4 border p-4 rounded-md"
       >
-        <Input placeholder="Enter lesson title" />
-      </Form.Item>
+        <Form.Item
+          label="Lesson Title"
+          name="title"
+          rules={[{ required: true, message: "Title is required" }]}
+        >
+          <Input placeholder="Enter lesson title" />
+        </Form.Item>
 
-      <Form.Item
-        label="Serial"
-        name="serial"
-        rules={[
-          { required: true, message: "Serial is required" },
-          {
-            type: "number",
-            message: "Serial must be a number",
-            transform: (value) => Number(value),
-          },
-        ]}
-      >
-        <Input type="number" placeholder="Enter lesson serial" />
-      </Form.Item>
+        <Form.Item
+          label="Serial"
+          name="serial"
+          rules={[
+            { required: true, message: "Serial is required" },
+            {
+              type: "number",
+              message: "Serial must be a number",
+              transform: (value) => Number(value),
+            },
+          ]}
+        >
+          <Input type="number" placeholder="Enter lesson serial" />
+        </Form.Item>
 
-      <Form.Item
-        label="Video URL"
-        name="videoUrl"
-        rules={[{ required: true, message: "Video URL is required" }]}
-      >
-        <Input.TextArea placeholder="Enter video URL" />
-      </Form.Item>
+        <Form.Item
+          label="Video URL"
+          name="videoUrl"
+          rules={[{ required: true, message: "Video URL is required" }]}
+        >
+          <Input.TextArea placeholder="Enter video URL" />
+        </Form.Item>
 
-      <Button onClick={() => setUploadVideo(true)}>Change video</Button>
+        <Button
+          type="default"
+          htmlType="button"
+          onClick={() => setUploadVideo(true)}
+        >
+          Change video
+        </Button>
+
+        <Form.Item
+          label="Video Length (Minutes)"
+          name="videoLength"
+          rules={[
+            { required: true, message: "Video length is required" },
+            {
+              type: "number",
+              message: "Video length must be a number",
+              transform: (value) => Number(value),
+            },
+          ]}
+        >
+          <Input type="number" placeholder="Enter video length in minutes" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            disabled={isLoading}
+            loading={isLoading}
+            iconPosition="end"
+            htmlType="submit"
+            className="w-full"
+          >
+            {isLoading ? "Updating..." : " Update Lesson"}
+          </Button>
+        </Form.Item>
+      </Form>
       <LessonVideoUploadModal
         open={uploadVideo}
         setOpen={setUploadVideo}
         setVideoUrl={setNewVideoUrl}
       />
-
-      <Form.Item
-        label="Video Length (Minutes)"
-        name="videoLength"
-        rules={[
-          { required: true, message: "Video length is required" },
-          {
-            type: "number",
-            message: "Video length must be a number",
-            transform: (value) => Number(value),
-          },
-        ]}
-      >
-        <Input type="number" placeholder="Enter video length in seconds" />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="w-full">
-          Update Lesson
-        </Button>
-      </Form.Item>
-    </Form>
+    </>
   );
 };
 
