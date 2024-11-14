f6cfaebd2a666b88b903a1b309261145f86c78fe
unknown <najimuddinhelalal670@gmail.com>
2024-11-14 22:49:38 +0600

    set up instractor sidebar

--- frontend/src/components/dashboard/sidebars/InstructorSidebar.tsx
+++ frontend/src/components/dashboard/sidebars/InstructorSidebar.tsx
@@ -1,13 +1,21 @@
+import LogoutButton from "@/components/shared/LogoutButton";
 import { Menu, MenuProps } from "antd/lib";
+import Link from "next/link";
 import {
   FaUserCircle,
   FaBookOpen,
   FaUserGraduate,
   FaTasks,
-  FaClipboardCheck,
   FaChartLine,
   FaCommentDots,
   FaSignOutAlt,
+  FaCalendarAlt,
+  FaChartPie,
+  FaChalkboardTeacher,
+  FaLayerGroup,
+  FaListAlt,
+  FaQuestionCircle,
+  FaFolderPlus,
 } from "react-icons/fa";
 
 const InstructorSidebar = () => {
@@ -15,42 +23,103 @@ const InstructorSidebar = () => {
     {
       key: "1",
       icon: <FaUserCircle />,
-      label: "Profile",
+      label: (
+        <Link href={"/dashboard/profile"}>Profile</Link>
+      ),
     },
     {
       key: "2",
       icon: <FaBookOpen />,
-      label: "My Courses",
+      label: (
+        <Link href={"/dashboard/my-classes"}>My Classes</Link>
+      ),
     },
     {
       key: "3",
       icon: <FaUserGraduate />,
-      label: "Students",
+      label: (
+        <Link href={"/dashboard/my-students"}>My Students</Link>
+      ),
     },
     {
       key: "4",
       icon: <FaTasks />,
-      label: "Assignments",
+      label: (
+        <Link href={"/dashboard/assignments"}>Assignments</Link>
+      ),
     },
     {
       key: "5",
-      icon: <FaClipboardCheck />,
-      label: "Grading",
+      icon: <FaLayerGroup />,
+      label: (
+        <Link href={"/dashboard/create-module"}>Create Module</Link>
+      ),
     },
     {
       key: "6",
-      icon: <FaChartLine />,
-      label: "Performance",
+      icon: <FaListAlt />,
+      label: (
+        <Link href={"/dashboard/create-lesson"}>Create Lesson</Link>
+      ),
     },
     {
       key: "7",
-      icon: <FaCommentDots />,
-      label: "Feedback",
+      icon: <FaQuestionCircle />,
+      label: (
+        <Link href={"/dashboard/create-quiz"}>Create Quiz</Link>
+      ),
     },
     {
       key: "8",
+      icon: <FaFolderPlus />,
+      label: "Manage Content",
+      children: [
+        {
+          key: "8-1",
+          icon: <FaLayerGroup />,
+          label: <Link href={"/dashboard/manage-modules"}>Modules</Link>,
+        },
+        {
+          key: "8-2",
+          icon: <FaListAlt />,
+          label: <Link href={"/dashboard/manage-lessons"}>Lessons</Link>,
+        },
+        {
+          key: "8-3",
+          icon: <FaQuestionCircle />,
+          label: <Link href={"/dashboard/manage-quizzes"}>Quizzes</Link>,
+        },
+      ],
+    },
+    {
+      key: "9",
+      icon: <FaChartLine />,
+      label: <Link href={"/dashboard/performance"}>Performance</Link>,
+    },
+    {
+      key: "10",
+      icon: <FaCalendarAlt />,
+      label: <Link href={"/dashboard/meetings"}>Meetings</Link>,
+    },
+    {
+      key: "11",
+      icon: <FaChartPie />,
+      label: <Link href={"/dashboard/analytics"}>Analytic</Link>,
+    },
+    {
+      key: "12",
+      icon: <FaChalkboardTeacher />,
+      label: <Link href={"/dashboard/teaching-resources"}>Teaching Resources</Link>,
+    },
+    {
+      key: "13",
+      icon: <FaCommentDots />,
+      label: <Link href={"/dashboard/feedback"}>Feedback</Link>,
+    },
+    {
+      key: "14",
       icon: <FaSignOutAlt />,
-      label: "Logout",
+      label: <LogoutButton />,
     },
   ];
 
---
+++ frontend/src/components/myClasses/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyClasses = () => {
+    return (
+        <div>
+          this is classes items
+        </div>
+    );
+};
+
+export default MyClasses;
