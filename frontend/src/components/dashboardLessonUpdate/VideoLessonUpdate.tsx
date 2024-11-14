bd25187e55a221ab788dbea1196083f40daa0487
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-14 12:38:02 +0600

    lesson update route and layout design done

---
+++ frontend/src/components/dashboardLessonUpdate/VideoLessonUpdate.tsx
@@ -0,0 +1,5 @@
+const VideoLessonUpdate = () => {
+  return <div></div>;
+};
+
+export default VideoLessonUpdate;
--- frontend/src/components/dashboardModulesLessons/modules/LessonItem.tsx
+++ frontend/src/components/dashboardModulesLessons/modules/LessonItem.tsx
@@ -4,12 +4,14 @@ import LessonVideo from "./video";
 import LessonIntro from "./introduction";
 import LessonAssignment from "./assignment";
 import LessonQuiz from "./quiz";
+import Link from "next/link";
 
 type Props = {
   lesson: ILesson;
 };
 
 const LessonItem = ({ lesson }: Props) => {
+  const path = `/dashboard/lesson/update/${lesson?.id}?id=${lesson?.id}&&lessonType=${lesson?.type}&lessonTitle=${lesson?.title}`;
   return (
     <div className="border rounded-md shadow-sm p-4 hover:bg-gray-50 transition duration-200">
       <div className="flex justify-between items-center mb-4">
@@ -17,12 +19,12 @@ const LessonItem = ({ lesson }: Props) => {
           {lesson?.serial}. {lesson?.title}
         </h5>
         <div className="flex gap-2">
-          <button
+          <Link
+            href={path}
             className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
-            onClick={() => alert(`Edit lesson: ${lesson?.title}`)}
           >
             <FiEdit size={16} />
-          </button>
+          </Link>
           <button
             className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
             onClick={() => alert(`Delete lesson: ${lesson?.title}`)}
