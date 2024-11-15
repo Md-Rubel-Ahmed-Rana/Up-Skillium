e4a42535dd612116ed8e33df1d61d40fd6f60392
unknown <najimuddinhelalal670@gmail.com>
2024-11-15 23:29:44 +0600

    get classes data from db of instructor dashboard

---
+++ frontend/src/components/myClasses/MyClassContainer.tsx
@@ -0,0 +1,19 @@
+import { ICourse } from "@/types/course.type";
+import MyClasseCard from "./MyClasseCard";
+
+type Props = {
+  courses: ICourse[];
+};
+
+const MyClassContainer = ({ courses }: Props) => {
+  console.log(courses);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {courses?.map((course) => (
+        <MyClasseCard key={course?.id} course={course} />
+      ))}
+    </div>
+  );
+};
+
+export default MyClassContainer;
