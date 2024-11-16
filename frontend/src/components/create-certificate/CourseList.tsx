bbc661b69fb2730ab43c76ed036c8747ca44d426
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-16 16:55:22 +0600

    Certificate create form design completed

--- frontend/src/components/create-certificate/CourseList.tsx
+++ frontend/src/components/create-certificate/CourseList.tsx
@@ -2,17 +2,37 @@ import { useGetAllCoursesQuery } from "@/features/course";
 import { ICourse } from "@/types/course.type";
 import { Button, Dropdown, MenuProps } from "antd/lib";
 
-const CourseList = () => {
+type Props = {
+  setSelectedCourse: (values: {
+    id: string;
+    name: string;
+    technologies: string[];
+  }) => void;
+};
+
+const CourseList = ({ setSelectedCourse }: Props) => {
   const { data } = useGetAllCoursesQuery({});
   const courses = data?.data as ICourse[];
+
+  const handleSelectCourse = (course: ICourse) => {
+    setSelectedCourse({
+      id: course?.id,
+      name: course?.title,
+      technologies: course?.technologies,
+    });
+  };
 
   const courseList: MenuProps["items"] =
     courses?.map((course) => ({
       key: course?.id,
-      label: <Button className="w-full">{course?.title}</Button>,
+      label: (
+        <Button onClick={() => handleSelectCourse(course)} className="w-full">
+          {course?.title}
+        </Button>
+      ),
     })) || [];
   return (
-    <Dropdown className="" menu={{ items: courseList }}>
+    <Dropdown className="w-full" menu={{ items: courseList }}>
       <Button className="font-semibold">Select Course</Button>
     </Dropdown>
   );
--- frontend/src/components/create-certificate/index.tsx
+++ frontend/src/components/create-certificate/index.tsx
@@ -1,33 +1,43 @@
+import { useState } from "react";
+import CertificateForm from "./CertificateForm";
 import CourseList from "./CourseList";
 import StudentList from "./StudentList";
 
 const CreateCertificate = () => {
-  const formData = {
-    certificatePdfData: {
-      studentName: "",
-      courseName: "",
-      technologies: [
-        "HTML",
-        "CSS",
-        "Bootstrap",
-        "Tailwindcss",
-        "Javascript",
-        "Reactjs",
-        "Nextjs",
-      ],
-      score: 80,
-    },
-    schema: {
-      user: "67278c03f343385e39a8b4e3",
-      course: "671a210c16006aa95c25c87b",
-    },
-  };
+  const [selectedStudent, setSelectedStudent] = useState({
+    id: "",
+    name: "",
+  });
+
+  const [selectedCourse, setSelectedCourse] = useState({
+    id: "",
+    name: "",
+    technologies: [""],
+  });
 
   return (
     <div className="flex justify-center items-center pt-5 pb-20">
-      <div>
-        <CourseList />
-        <StudentList />
+      <div className="flex w-full gap-10 p-5 border rounded-md">
+        <CertificateForm
+          selectedCourse={selectedCourse}
+          selectedStudent={selectedStudent}
+        />
+        <div className="border w-full flex gap-2 p-5 rounded-md">
+          <div className="w-full">
+            <StudentList setSelectedStudent={setSelectedStudent} />
+            <div className="mt-2">
+              <h2 className="text-gray-700">Selected Student:</h2>
+              <h3 className="font-semibold">{selectedStudent?.name}</h3>
+            </div>
+          </div>
+          <div className="w-full">
+            <CourseList setSelectedCourse={setSelectedCourse} />
+            <div className="mt-2">
+              <h2 className="text-gray-700">Selected Course:</h2>
+              <h3 className="font-semibold">{selectedCourse?.name}</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
