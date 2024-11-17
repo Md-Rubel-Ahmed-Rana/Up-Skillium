9682fc1462cc4be75c52d2753a107e73795d19b9
unknown <najimuddinhelalal670@gmail.com>
2024-11-17 20:35:58 +0600

    set up page layout and display basic student table on MyStudent page

---
+++ frontend/src/components/dashboard/my-students/index.tsx
@@ -0,0 +1,11 @@
+import StudentsListContainer from "./StudentsListContainer";
+
+const MyStudents = () => {
+  return (
+    <div>
+      <StudentsListContainer />
+    </div>
+  );
+};
+
+export default MyStudents;
--- frontend/src/components/myClasses/MyClassContainer.tsx
+++ frontend/src/components/myClasses/MyClassContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const MyClassContainer = ({ courses }: Props) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2 px-3">
       {courses?.map((course) => (
         <MyClassCard key={course?.id} course={course} />
       ))}
