e1ff20d4ba5ebea3532c4acaa4e3d640c85d8312
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-20 16:05:10 +0600

    Lessons showing on modal of a specifix module completed

--- frontend/src/components/manageModules/ViewLessonsModal.tsx
+++ frontend/src/components/manageModules/ViewLessonsModal.tsx
@@ -1,4 +1,6 @@
-import { Modal } from "antd/lib";
+import { useGetLessonsByModuleIdQuery } from "@/features/lesson";
+import { ILesson } from "@/types/lesson.type";
+import { Modal, Tag } from "antd/lib";
 
 type Props = {
   moduleId: string;
@@ -8,15 +10,30 @@ type Props = {
 };
 
 const ViewLessonsModal = ({ open, setOpen, moduleName, moduleId }: Props) => {
+  const { data, isLoading } = useGetLessonsByModuleIdQuery({ moduleId });
+  const lessons = data?.data as ILesson[];
   return (
     <Modal
       footer={false}
       open={open}
       onCancel={() => setOpen(false)}
       title={`Lessons for: ${moduleName}`}
+      loading={isLoading}
     >
-      <h2>{moduleName}</h2>
-      <h2>{moduleId}</h2>
+      <div className="flex flex-col gap-3">
+        {lessons.map((lesson) => (
+          <div
+            className="flex justify-between gap-3 border p-2 rounded-md bg-gray-50"
+            key={lesson?.id}
+          >
+            <div className="flex items-center ga-2">
+              <Tag color="geekblue">S. {lesson?.serial}</Tag>
+              <h3>{lesson?.title}</h3>
+            </div>
+            <Tag color="geekblue">{lesson?.type}</Tag>
+          </div>
+        ))}
+      </div>
     </Modal>
   );
 };
