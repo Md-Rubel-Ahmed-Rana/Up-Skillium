bbdea67c8ceb5f79513821e3a0d658899af922ba
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-22 00:08:17 +0600

    All the quizzes displayed by fetching data from server via API integrating

--- frontend/src/components/manageQuizzes/index.tsx
+++ frontend/src/components/manageQuizzes/index.tsx
@@ -7,21 +7,6 @@ import QuizUpdateModal from "./QuizUpdateModal";
 const ManageQuizzes = () => {
   const { data, isLoading } = useGetAllQuizzesQuery({});
   const quizzes = data?.data as IGetQuizQuestion[];
-  const lessons: IGetQuizQuestion[] = Array.from({ length: 100 }).map(
-    (_, index) => ({
-      id: (index + 1).toString(),
-      question: `Question ${index + 1}`,
-      correctAnswer: `Correct answer ${index + 1}`,
-      options: ["option 1", "option 2", "option 3", "option 4"],
-      module: {
-        id: (index + 1).toString(),
-        title: `Module ${index + 1}`,
-      },
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    })
-  );
-
   const columns: TableProps<IGetQuizQuestion>["columns"] = [
     {
       title: "Question",
@@ -79,7 +64,7 @@ const ManageQuizzes = () => {
       <h2 className="text-lg lg:text-2xl font-semibold mb-3">Manage Quizzes</h2>
       <Table
         columns={columns}
-        dataSource={lessons}
+        dataSource={quizzes}
         bordered
         pagination={{ pageSize: 10 }}
         className="shadow-md rounded-lg w-full min-w-[900px]"
