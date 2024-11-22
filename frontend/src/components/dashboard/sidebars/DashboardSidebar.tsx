24e7709dd762005f12d283b61d8377d8731c5eeb
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-22 23:39:23 +0600

    Manage users item added on admin dashboard's sidebar

--- frontend/src/components/dashboard/sidebars/DashboardSidebar.tsx
+++ frontend/src/components/dashboard/sidebars/DashboardSidebar.tsx
@@ -2,23 +2,28 @@ import { useGetLoggedInUserQuery } from "@/features/auth";
 import DashboardSidebarSkeleton from "@/skeletons/dashboardSidebarSkeleton";
 import { IUser } from "@/types/user.type";
 import AdminSidebar from "./AdminSidebar";
+import DefaultSidebar from "./DefaultSidebar";
 import InstructorSidebar from "./InstructorSidebar";
 import StudentSidebar from "./StudentSidebar";
 
 const DashboardSidebar = () => {
   const { data, isLoading } = useGetLoggedInUserQuery({});
   const user = data?.data as IUser;
+
+  const renderDashboard =
+    user && user?.role?.name === "student" ? (
+      <StudentSidebar />
+    ) : user?.role?.name === "admin" ? (
+      <AdminSidebar />
+    ) : user?.role?.name === "instructor" ? (
+      <InstructorSidebar />
+    ) : (
+      <DefaultSidebar />
+    );
+
   return (
     <>
-      {isLoading ? (
-        <DashboardSidebarSkeleton />
-      ) : (
-        <div>
-          {user?.role?.name === "student" && <StudentSidebar />}
-          {user?.role?.name === "admin" && <AdminSidebar />}
-          {user?.role?.name === "instructor" && <InstructorSidebar />}
-        </div>
-      )}
+      {isLoading ? <DashboardSidebarSkeleton /> : <div>{renderDashboard}</div>}
     </>
   );
 };
