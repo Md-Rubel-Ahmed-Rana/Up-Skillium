59f55a530fc4f992cdbc72b53640c27ea7d2b496
unknown <najimuddinhelalal670@gmail.com>
2024-11-26 17:30:49 +0600

    added recommended related data from db

--- frontend/src/components/courseDetails/index.tsx
+++ frontend/src/components/courseDetails/index.tsx
@@ -1,16 +1,20 @@
 import CourseBasicDetails from "./courseBasicDetails";
 import CourseOutline from "./courseOutline";
+import RecommrndedCourses from "./recommendedCourses";
 
 const CourseDetails = () => {
   return (
-    <div className="flex lg:flex-row flex-col-reverse justify-between lg:gap-5 gap-20 lg:p-5">
-      <div className="w-full lg:w-2/3">
-        <CourseBasicDetails />
+    <>
+      <div className="flex lg:flex-row flex-col-reverse justify-between lg:gap-5 gap-20 lg:p-5">
+        <div className="w-full lg:w-2/3">
+          <CourseBasicDetails />
+        </div>
+        <div className="w-full lg:w-1/3 lg:border-0 pb-10 lg:pb-0 border-b-2">
+          <CourseOutline />
+        </div>
       </div>
-      <div className="w-full lg:w-1/3 lg:border-0 pb-10 lg:pb-0 border-b-2">
-        <CourseOutline />
-      </div>
-    </div>
+      <RecommrndedCourses />
+    </>
   );
 };
 
