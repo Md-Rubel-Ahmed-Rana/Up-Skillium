526d04fbedb33650ef6991c6d143f02bad702d8d
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-27 17:39:15 +0600

    Showing total enrolled students number on course details page done

--- frontend/src/components/courseDetails/courseBasicDetails/CourseTitleCategoryCard.tsx
+++ frontend/src/components/courseDetails/courseBasicDetails/CourseTitleCategoryCard.tsx
@@ -1,18 +1,26 @@
-import { Card, Typography } from "antd/lib";
+import { Button, Card, Typography } from "antd/lib";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
 const { Title, Text } = Typography;
 
 type Props = {
   title: string;
   category: string;
+  totalStudents: number;
 };
 
-const CourseTitleCategoryCard = ({ title, category }: Props) => {
+const CourseTitleCategoryCard = ({ title, category, totalStudents }: Props) => {
   return (
-    <Card bordered={false} className="rounded-lg">
+    <Card bordered={false} className="rounded-lg p-4">
       <Title level={3} className="text-blue-600">
         {title}
       </Title>
-      <Text className="text-gray-500">{category}</Text>
+      <Text className="text-gray-500 block mb-2">{category}</Text>
+      <Button
+        icon={<AiOutlineUsergroupAdd className="text-green-500 text-lg" />}
+        className="text-green-500 font-medium flex items-center gap-2"
+      >
+        {totalStudents?.toLocaleString() || 0} students enrolled
+      </Button>
     </Card>
   );
 };
