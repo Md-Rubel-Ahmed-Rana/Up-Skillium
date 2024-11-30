38aae6a1ed5a8ff8909bf76083cde4f4f24efbad
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-30 11:56:25 +0600

    Welcome and text-changers implmentation done

--- frontend/src/components/home/banner/index.tsx
+++ frontend/src/components/home/banner/index.tsx
@@ -1,10 +1,14 @@
 import HeadlineStreaming from "./HeadlineStreaming";
+import ImpressiveTextChanger from "./ImpressiveTextChanger";
+import NavigatorButton from "./NavigatorButton";
 import TotalCounters from "./TotalCounters";
 
 const Banner = () => {
   return (
-    <div>
+    <div className="w-full bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 pb-20 text-white">
       <HeadlineStreaming />
+      <ImpressiveTextChanger />
+      <NavigatorButton />
       <TotalCounters />
     </div>
   );
