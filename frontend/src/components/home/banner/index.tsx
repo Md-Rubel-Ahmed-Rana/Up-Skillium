82128508ee289ad25be07d18acb1d10ee986743a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-11-30 11:26:41 +0600

    Total counters component design and development done

--- frontend/src/components/home/banner/index.tsx
+++ frontend/src/components/home/banner/index.tsx
@@ -1,9 +1,11 @@
 import HeadlineStreaming from "./HeadlineStreaming";
+import TotalCounters from "./TotalCounters";
 
 const Banner = () => {
   return (
     <div>
       <HeadlineStreaming />
+      <TotalCounters />
     </div>
   );
 };
