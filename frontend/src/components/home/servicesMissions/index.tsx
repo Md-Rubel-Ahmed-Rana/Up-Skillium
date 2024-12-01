dc0feb2acccc5f7c1c9c2d7c97c863f0db36fdc0
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-12-01 12:54:05 +0600

    popular courses page design done

--- frontend/src/components/home/servicesMissions/index.tsx
+++ frontend/src/components/home/servicesMissions/index.tsx
@@ -1,18 +1,11 @@
 import ServiceContainer from "./ServiceContainer";
+import ServiceHeader from "./ServiceHeader";
 
 const OurServicesAndMissions = () => {
   return (
     <section className="w-full bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600  text-white py-20 relative">
       <div className="container mx-auto px-6">
-        <h2 className="text-xl lg:text-4xl font-bold text-center text-white mb-4">
-          Our Dedicated Services & Mission
-        </h2>
-        <p className="text-center text-white mb-12 max-w-2xl mx-auto">
-          Empowering learners with comprehensive services designed to transform
-          education into a journey of growth and success. Explore how our
-          platform goes beyond traditional learning to support every step of
-          your educational journey.
-        </p>
+        <ServiceHeader />
         <ServiceContainer />
       </div>
     </section>
