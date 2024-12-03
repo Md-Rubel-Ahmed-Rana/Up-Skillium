a9a0757cf37487a0d660a0d2043a79ad1820c863
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-12-03 12:16:45 +0600

    Homepage section title and description component make common and reusable

--- frontend/src/components/home/popularCourses/PopularCourseHeader.tsx
+++ frontend/src/components/home/popularCourses/PopularCourseHeader.tsx
@@ -1,26 +1,12 @@
-import useTittleDescAnimation from "@/hooks/useTittleDescAnimation";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import SectionHeader from "../SectionHeader";
 
 const PopularCourseHeader = () => {
-  const ref = useRef(null);
-  const { title, desc } = useTittleDescAnimation(ref);
   return (
-    <div ref={ref} className="flex flex-col justify-center items-center">
-      <motion.h1
-        {...title}
-        className="text-lg lg:text-3xl text-center font-bold"
-      >
-        Explore Our Most Popular Courses
-      </motion.h1>
-      <motion.p
-        {...desc}
-        className="text-sm lg:text-lg w-full lg:w-1/2 mx-auto text-center text-gray-300 mt-4"
-      >
-        Discover top-rated courses crafted by industry experts. <br /> Boost
-        your skills and career with our best-selling programs.
-      </motion.p>
-    </div>
+    <SectionHeader
+      titleText="Explore Our Most Popular Courses"
+      descText="Discover top-rated courses crafted by industry experts. Boost
+        your skills and career with our best-selling programs."
+    />
   );
 };
 
--- frontend/src/components/home/servicesMissions/ServiceHeader.tsx
+++ frontend/src/components/home/servicesMissions/ServiceHeader.tsx
@@ -1,28 +1,14 @@
-import useTittleDescAnimation from "@/hooks/useTittleDescAnimation";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import SectionHeader from "../SectionHeader";
 
 const ServiceHeader = () => {
-  const ref = useRef(null);
-  const { title, desc } = useTittleDescAnimation(ref);
   return (
-    <div ref={ref}>
-      <motion.h1
-        {...title}
-        className="text-xl lg:text-4xl font-bold text-center text-white mb-4"
-      >
-        Our Dedicated Services & Mission
-      </motion.h1>
-      <motion.p
-        {...desc}
-        className="text-center text-gray-300 mb-12 max-w-2xl mx-auto"
-      >
-        Empowering learners with comprehensive services designed to transform
+    <SectionHeader
+      titleText="Our Dedicated Services & Mission"
+      descText="Empowering learners with comprehensive services designed to transform
         education into a journey of growth and success. Explore how our platform
         goes beyond traditional learning to support every step of your
-        educational journey.
-      </motion.p>
-    </div>
+        educational journey."
+    />
   );
 };
 
