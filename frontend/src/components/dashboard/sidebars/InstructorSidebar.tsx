eeba4816b6fdf0f8f21b975db35d6c7b45b74144
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-12-14 16:41:23 +0600

    Instructor dashboard sidebar desigin done

--- frontend/src/components/dashboard/sidebars/InstructorSidebar.tsx
+++ frontend/src/components/dashboard/sidebars/InstructorSidebar.tsx
@@ -4,126 +4,234 @@ import { Menu, MenuProps } from "antd/lib";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import {
+  FaAward,
+  FaBook,
   FaBookOpen,
-  FaCalendarAlt,
-  FaChalkboardTeacher,
-  FaChartLine,
-  FaChartPie,
+  FaCheckCircle,
+  FaClipboardList,
   FaCommentDots,
-  FaFolderPlus,
-  FaLayerGroup,
-  FaListAlt,
-  FaQuestionCircle,
+  FaFileAlt,
+  FaFolderOpen,
+  FaHourglassHalf,
+  FaPenFancy,
+  FaPenSquare,
+  FaPlusCircle,
+  FaPlusSquare,
   FaSignOutAlt,
   FaTasks,
   FaUserCircle,
-  FaUserGraduate,
 } from "react-icons/fa";
+import { HiOutlineDocumentText } from "react-icons/hi";
+import {
+  MdAddToQueue,
+  MdDoneAll,
+  MdHistory,
+  MdOutlineLibraryBooks,
+  MdOutlineMeetingRoom,
+  MdPassword,
+  MdSchedule,
+  MdVideoCameraFront,
+} from "react-icons/md";
 
 const InstructorSidebar = () => {
   const router = useRouter();
-  const InstructorItems: MenuProps["items"] = [
+  const adminItems: MenuProps["items"] = [
     {
       key: "1",
       icon: <FaUserCircle />,
-      label: <Link href={"/dashboard/profile-info"}>Profile</Link>,
+      label: "Profile",
+      children: [
+        {
+          key: "1-1",
+          icon: <FaUserCircle />,
+          label: <Link href="/dashboard/profile-info">Profile Info</Link>,
+        },
+        {
+          key: "1-2",
+          icon: <MdPassword />,
+          label: <Link href="/dashboard/change-password">Change Password</Link>,
+        },
+      ],
     },
     {
       key: "2",
-      icon: <FaBookOpen />,
-      label: <Link href={"/dashboard/my-classes"}>My Classes</Link>,
+      icon: <FaBook />,
+      label: <Link href="/dashboard/my-classes">My Classes</Link>,
     },
     {
       key: "3",
-      icon: <FaUserGraduate />,
-      label: <Link href={"/dashboard/my-students"}>My Students</Link>,
+      icon: <MdOutlineLibraryBooks />,
+      label: "Manage Content",
+      children: [
+        {
+          key: "3-1",
+          icon: <FaFolderOpen />,
+          label: <Link href="/dashboard/modules">All Modules</Link>,
+        },
+        {
+          key: "3-2",
+          icon: <FaPlusSquare />,
+          label: <Link href="/dashboard/create-module">Create Module</Link>,
+        },
+        {
+          key: "3-3",
+          icon: <FaBookOpen />,
+          label: <Link href="/dashboard/lessons">All Lessons</Link>,
+        },
+        {
+          key: "3-4",
+          icon: <FaPlusCircle />,
+          label: <Link href="/dashboard/create-lesson">Create Lesson</Link>,
+        },
+        {
+          key: "3-5",
+          icon: <FaTasks />,
+          label: <Link href="/dashboard/quizzes">All Quizzes</Link>,
+        },
+        {
+          key: "3-6",
+          icon: <FaPenFancy />,
+          label: <Link href="/dashboard/create-quiz">Create Quiz</Link>,
+        },
+        {
+          key: "3-7",
+          icon: <HiOutlineDocumentText />,
+          label: (
+            <Link href="/dashboard/course-outlines">Courses Outlines</Link>
+          ),
+        },
+        {
+          key: "3-8",
+          icon: <FaPlusCircle />,
+          label: (
+            <Link href="/dashboard/create-course-outline">Create Outline</Link>
+          ),
+        },
+      ],
     },
     {
       key: "4",
       icon: <FaTasks />,
-      label: <Link href={"/dashboard/assignments"}>Assignments</Link>,
+      label: "Assignments",
+      children: [
+        {
+          key: "4-1",
+          icon: <FaClipboardList />,
+          label: <Link href="/dashboard/assignments">All Assignments</Link>,
+        },
+        {
+          key: "4-2",
+          icon: <FaHourglassHalf />,
+          label: (
+            <Link href="/dashboard/pending-assignments">
+              Pending Assignments
+            </Link>
+          ),
+        },
+        {
+          key: "4-3",
+          icon: <FaCheckCircle />,
+          label: (
+            <Link href="/dashboard/reviewed-assignments">
+              Reviewed Assignments
+            </Link>
+          ),
+        },
+        {
+          key: "4-4",
+          icon: <FaPenSquare />,
+          label: (
+            <Link href="/dashboard/create-assignment">Create Assignment</Link>
+          ),
+        },
+      ],
     },
     {
       key: "5",
-      icon: <FaLayerGroup />,
-      label: <Link href={"/dashboard/create-module"}>Create Module</Link>,
+      icon: <MdVideoCameraFront />,
+      label: "Live Classes",
+      children: [
+        {
+          key: "5-1",
+          icon: <MdHistory />,
+          label: <Link href="/dashboard/previous-live-classes">Previous</Link>,
+        },
+        {
+          key: "5-2",
+          icon: <MdSchedule />,
+          label: <Link href="/dashboard/upcoming-live-classes">Upcoming</Link>,
+        },
+        {
+          key: "5-3",
+          icon: <MdAddToQueue />,
+          label: (
+            <Link href="/dashboard/create-live-class">Create Live Class</Link>
+          ),
+        },
+      ],
     },
     {
       key: "6",
-      icon: <FaListAlt />,
-      label: <Link href={"/dashboard/create-lesson"}>Create Lesson</Link>,
+      icon: <FaAward />,
+      label: "Certificates",
+      children: [
+        {
+          key: "6-1",
+          icon: <FaFileAlt />,
+          label: <Link href="/dashboard/certificates">Certificates</Link>,
+        },
+        {
+          key: "6-2",
+          icon: <FaPlusSquare />,
+          label: (
+            <Link href="/dashboard/create-certificate">Create Certificate</Link>
+          ),
+        },
+      ],
     },
     {
       key: "7",
-      icon: <FaQuestionCircle />,
-      label: <Link href={"/dashboard/create-quiz"}>Create Quiz</Link>,
+      icon: <FaCommentDots />,
+      label: <Link href="/dashboard/reviews">Reviews</Link>,
     },
     {
       key: "8",
-      icon: <FaFolderPlus />,
-      label: "Manage Content",
+      icon: <MdOutlineMeetingRoom />,
+      label: "Manage Meetings",
       children: [
         {
           key: "8-1",
-          icon: <FaLayerGroup />,
-          label: <Link href={"/dashboard/manage-modules"}>Modules</Link>,
+          icon: <MdSchedule />,
+          label: (
+            <Link href="/dashboard/meetings/upcoming">Upcoming Meetings</Link>
+          ),
         },
         {
           key: "8-2",
-          icon: <FaListAlt />,
-          label: <Link href={"/dashboard/manage-lessons"}>Lessons</Link>,
-        },
-        {
-          key: "8-3",
-          icon: <FaQuestionCircle />,
-          label: <Link href={"/dashboard/manage-quizzes"}>Quizzes</Link>,
+          icon: <MdDoneAll />,
+          label: (
+            <Link href="/dashboard/meetings/completed">Completed Meetings</Link>
+          ),
         },
       ],
     },
     {
       key: "9",
-      icon: <FaChartLine />,
-      label: <Link href={"/dashboard/performance"}>Performance</Link>,
-    },
-    {
-      key: "10",
-      icon: <FaCalendarAlt />,
-      label: <Link href={"/dashboard/meetings"}>Meetings</Link>,
-    },
-    {
-      key: "11",
-      icon: <FaChartPie />,
-      label: <Link href={"/dashboard/analytics"}>Analytic</Link>,
-    },
-    {
-      key: "12",
-      icon: <FaChalkboardTeacher />,
-      label: (
-        <Link href={"/dashboard/teaching-resources"}>Teaching Resources</Link>
-      ),
-    },
-    {
-      key: "13",
-      icon: <FaCommentDots />,
-      label: <Link href={"/dashboard/feedback"}>Feedback</Link>,
-    },
-    {
-      key: "14",
       icon: <FaSignOutAlt />,
       label: <LogoutButton />,
     },
   ];
 
-  const pathToKeyMap = buildPathToKeyMap(InstructorItems);
+  const pathToKeyMap = buildPathToKeyMap(adminItems);
   const selectedKey = pathToKeyMap[router.pathname] || "1";
 
   return (
     <Menu
       theme="light"
-      className="w-full lg:min-h-screen h-full mt-3 flex lg:flex-col flex-row overflow-x-auto lg:overflow-visible space-x-3 lg:space-x-0"
       mode="inline"
-      selectedKeys={[selectedKey]}
-      items={InstructorItems}
+      className="w-full lg:min-h-screen h-full mt-3 flex lg:flex-col flex-row overflow-x-auto lg:overflow-visible space-x-3 lg:space-x-0"
+      defaultSelectedKeys={[selectedKey]}
+      items={adminItems}
     />
   );
 };
--- frontend/src/components/myClasses/MyClassContainer.tsx
+++ frontend/src/components/myClasses/MyClassContainer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const MyClassContainer = ({ courses }: Props) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2 px-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-3 pb-20">
       {courses?.map((course) => (
         <MyClassCard key={course?.id} course={course} />
       ))}
