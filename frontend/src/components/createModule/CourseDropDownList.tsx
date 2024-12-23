01bb26d1491bc72bff405dd9d3aec310b49a9ac6
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-12-23 22:56:44 +0600

    Course list shown conditionally on create module page

--- frontend/src/components/createModule/CourseDropDownList.tsx
+++ frontend/src/components/createModule/CourseDropDownList.tsx
@@ -1,14 +1,25 @@
-import { useGetAllCoursesQuery } from "@/features/course";
+import { useGetLoggedInUserQuery } from "@/features/auth";
+import {
+  useGetAllCoursesQuery,
+  useGetInstructorCoursesQuery,
+} from "@/features/course";
 import { ICourse } from "@/types/course.type";
+import { IUser } from "@/types/user.type";
 import { Button, Dropdown, MenuProps } from "antd/lib";
 import Link from "next/link";
 
 const CourseDropDownList = () => {
+  const { data: userData } = useGetLoggedInUserQuery({});
+  const user = userData?.data as IUser;
+  const { data: instructorCourse } = useGetInstructorCoursesQuery({
+    instructorId: user?.id,
+  });
+  const instructorCourses = instructorCourse?.data as ICourse[];
   const { data } = useGetAllCoursesQuery({});
-  const courses = data?.data as ICourse[];
+  const adminCourses = data?.data as ICourse[];
 
-  const courseList: MenuProps["items"] =
-    courses?.map((course) => ({
+  const adminCourseList: MenuProps["items"] =
+    adminCourses?.map((course) => ({
       key: course?.id,
       label: (
         <Link
@@ -19,9 +30,24 @@ const CourseDropDownList = () => {
       ),
     })) || [];
 
+  const instructorCourseList: MenuProps["items"] =
+    instructorCourses?.map((course) => ({
+      key: course?.id,
+      label: (
+        <Link
+          href={`/dashboard/create-module/?courseId=${course?.id}&courseTitle=${course?.title}&category=${course?.category}&description=${course?.description}`}
+        >
+          {course?.title}
+        </Link>
+      ),
+    })) || [];
+
+  const courses =
+    user?.role?.name === "admin" ? adminCourseList : instructorCourseList;
+
   return (
     <div>
-      <Dropdown menu={{ items: courseList }}>
+      <Dropdown menu={{ items: courses }}>
         <Button className="font-semibold">Select Course</Button>
       </Dropdown>
     </div>
