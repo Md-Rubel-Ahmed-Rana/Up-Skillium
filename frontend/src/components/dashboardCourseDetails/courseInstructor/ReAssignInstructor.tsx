f0b38deb9a53e0596422c309787e6d2f735432a9
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-03-25 00:35:14 +0600

    undefined error resolved in dashboard/course details page

--- frontend/src/components/dashboardCourseDetails/courseInstructor/ReAssignInstructor.tsx
+++ frontend/src/components/dashboardCourseDetails/courseInstructor/ReAssignInstructor.tsx
@@ -12,8 +12,8 @@ const ReAssignInstructor = () => {
   const courseId = query?.courseId as string;
   const [isReAssign, setIsReAssign] = useState(false);
   const { data, isLoading } = useGetAllUsersQuery({});
-  const instructors = data?.data as IUser[];
-  const instructorUsers = instructors.filter(
+  const instructors = (data?.data as IUser[]) || [];
+  const instructorUsers = instructors?.filter(
     (user) => user?.role?.name === "instructor"
   );
 
