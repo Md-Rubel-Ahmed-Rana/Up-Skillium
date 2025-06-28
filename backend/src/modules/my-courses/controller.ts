b37e4d52c775e76255da0951fb5348ed42196699
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-28 10:45:31 +0600

    student course progress analytics api design done

--- backend/src/modules/my-courses/controller.ts
+++ backend/src/modules/my-courses/controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import RootController from "../../shared/rootController";
 import { MyCourseService } from "./service";
 import { Types } from "mongoose";
+import { IAnalyticFilters } from "./interface";
 
 class Controller extends RootController {
   getMyCourses = this.catchAsync(async (req: Request, res: Response) => {
@@ -38,6 +39,32 @@ class Controller extends RootController {
       data: null,
     });
   });
+
+  getStudentCourseProgressAnalyticsSummary = this.catchAsync(
+    async (req: Request, res: Response) => {
+      const { userId, courseId } = req.query as IAnalyticFilters;
+
+      const filters: Record<string, any> = {};
+
+      if (userId && Types.ObjectId.isValid(userId)) {
+        filters.user = new Types.ObjectId(userId);
+      }
+
+      if (courseId && Types.ObjectId.isValid(courseId)) {
+        filters.course = new Types.ObjectId(courseId);
+      }
+
+      const summary =
+        await MyCourseService.getStudentCourseProgressAnalyticsSummary(filters);
+
+      this.apiResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Student course progress analytics retrieved successfully",
+        data: summary,
+      });
+    }
+  );
 }
 
 export const MyCourseController = new Controller();
