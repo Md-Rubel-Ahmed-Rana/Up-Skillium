bb834049912a7ba89680ddeb44413daccbfcd240
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-29 09:22:26 +0600

    showing total cart items number on cart icon done

--- frontend/src/components/cart/CartDrawer.tsx
+++ frontend/src/components/cart/CartDrawer.tsx
@@ -1,14 +1,17 @@
+import { ICart } from "@/types/cart.type";
 import { Drawer } from "antd/lib";
 
 type Props = {
   open: boolean;
+  isLoading: boolean;
   setOpen: (value: boolean) => void;
+  items: ICart[];
 };
 
-const CartDrawer = ({ open, setOpen }: Props) => {
+const CartDrawer = ({ open, setOpen, isLoading, items }: Props) => {
   return (
     <Drawer
-      title="Basic Drawer"
+      title="Courses"
       closable={{ "aria-label": "Close Button" }}
       onClose={() => setOpen(false)}
       open={open}
