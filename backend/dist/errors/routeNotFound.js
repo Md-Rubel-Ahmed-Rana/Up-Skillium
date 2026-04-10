87b38a5570dc7d62550bbcaa4895e3c0227a3376
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2026-04-11 02:14:21 +0600

    remove dist from tracking and update gitignore

--- backend/dist/errors/routeNotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.expressRouteNotFound = void 0;
-const expressRouteNotFound = (app) => {
-    return app.use((req, res, next) => {
-        return res.sendFile("notFound.html", { root: "public" });
-    });
-};
-exports.expressRouteNotFound = expressRouteNotFound;
--- backend/dist/utils/extractFilePath.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const extractFilePath = (url) => {
-    const pathMatch = url.match(/(?<=up-skillium\.appspot\.com\/)(.*?)(?=\?)/);
-    const filePath = pathMatch && pathMatch[0];
-    return filePath;
-};
-exports.default = extractFilePath;
