ecf613eeefde14cb113a8e41150771850286ca39
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2026-04-10 23:16:42 +0600

    remove dist from tracking and update gitignore

--- backend/dist/shared/appHealthCheck.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthCheckRoute = void 0;
-const healthCheckRoute = (app) => {
-    app.get("/", (req, res) => {
-        res.sendFile("index.html");
-    });
-};
-exports.healthCheckRoute = healthCheckRoute;
--- backend/dist/utils/incrementAverageRating.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const incrementAverageRating = (totalReviews, averageRating, newRating) => {
-    const updatedTotalReviews = totalReviews + 1;
-    const updatedAverageRating = (averageRating * totalReviews + newRating) / updatedTotalReviews;
-    return parseFloat(updatedAverageRating.toFixed(2));
-};
-exports.default = incrementAverageRating;
